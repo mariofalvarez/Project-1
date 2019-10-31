c162f0c261fd2c4a7baf6dc2d8d36f6a7eb2701d
mariofalvarez <mariofalvarezjr@gmail.com>
2019-10-30 19:26:20 -0700

    linked firebase sdk

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  var firebaseConfig = {
+    apiKey: "AIzaSyATB51a2W2gJk5B0_xzLMPoH6yw2bV5WuI",
+    authDomain: "project-1-76180.firebaseapp.com",
+    databaseURL: "https://project-1-76180.firebaseio.com",
+    projectId: "project-1-76180",
+    storageBucket: "project-1-76180.appspot.com",
+    messagingSenderId: "280561902099",
+    appId: "1:280561902099:web:4c6157d4f389b12bd795e6"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  const db = firebase.initializeApp(firebaseConfig);
+});
