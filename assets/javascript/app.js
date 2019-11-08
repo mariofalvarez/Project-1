8e537cad2b13d5baec7b6e0f229ec694069d2a77
Herman Liu <hermanliu95@gmail.com>
2019-11-08 13:52:31 -0800

    the latest

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
   var firebaseConfig = {
     apiKey: "AIzaSyATB51a2W2gJk5B0_xzLMPoH6yw2bV5WuI",
     authDomain: "project-1-76180.firebaseapp.com",
@@ -19,7 +19,7 @@ $('.btn').on('click', function (e) {
 
   const weatherUrl = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=c2e38863824c4fbaa3e29a7d10f11bbf`;
   const queryUrl = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=SeGSW2oWcueAXwOb0zleD2J63hEIWBjY&city=${newCity}`;
-  
+
   // Weather api
   $.ajax({
     method: "GET",
@@ -28,40 +28,66 @@ $('.btn').on('click', function (e) {
     $('.weatherInfo').append(Math.floor((((response.data[0].temp) * 9) / 5) + 32));
     $('.weatherInfo').append(`<div><span>&#8457;</span></div>`);
   });
-  
+
+
   // Ticketmaster api
   $.ajax({
     url: queryUrl,
     method: "GET"
   }).then(data => {
     const results = data._embedded.events;
+    console.log(results);
+
     for (var i = 0; i < results.length; i++) {
       let eventDiv = $("<div>");
       let eventName = results[i].name;
       let eventImg = $("<img>");
-      let addBtn = $("<button>Add To Calendar</button>");
+      let addBtn = $("<button>Add To Event List</button>");
       let eventDateTime = results[i].dates.start.dateTime;
       let convertedDT = moment(eventDateTime).format("LLLL");
-      addBtn.addClass("addToCalendar");
+      addBtn.addClass("addToEventList");
+      addBtn.attr('data', eventName);
       eventImg.attr("src", results[i].images[i].url);
       eventImg.width("64px");
       eventImg.height("64px");
-       eventDiv.append(eventImg, eventName, convertedDT, addBtn);
-      $('.event-container').append(eventDiv);
+      eventImg.addClass('img'); //added a class to the img 
+      eventDiv.addClass('addToEvent');  //created a class to the eventDiv with the class name addToEvent
+      eventDiv.append(eventImg, eventName, convertedDT, addBtn);
+      console.log(eventDiv);
+
+      $('.event-container').prepend(eventDiv);
     }
   });
   document.getElementById('text').value = "";
 });
 
+//Function that holds all the data of the event
+function eventList() {
+
+}
+
 var events = [];
-$(".event-container").on("click", ".addToCalendar", function () {
-  console.log("Sports are lame");
+$(".event-container").on("click", ".addToEventList", function () {
+  //Create a var to hold the event name, location, and the time
+  let eventName = $('.addToEvent');
+  
+  //Gtrab the event and append it to a new table cell
+
+  //Create a new table cell which holds all the data.
+  var newRow = $('<tr>').append(
+    $('<td>').text(eventName),
+    $('<td>').text(eventLocation),
+    $('<td>').text(eventTime),
+  );
+
+  //Append the new row to the table
+  $('#eventList > tbody').append(newRow);
 
 });
 
 $("#searchBtn").on("click", function (e) {
   e.preventDefault();
-  console.log("searchEvents");
+  //console.log("searchEvents");
   // if the word in the search bar is contained in the name of the event
   // show event.
 });
